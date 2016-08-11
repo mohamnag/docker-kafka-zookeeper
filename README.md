a79b8255470bed5cf3e853e8e6d564f6abf0d081
Mohammad Naghavi <mohamnag@gmail.com>
2016-08-11 14:45:43 +0200

    adding zookeeper to the image too

---
+++ README.md
@@ -0,0 +1,8 @@
+# Kafka & Zookeeper in one image
+This image is **NOT** for production use but rather for testing purposes.
+
+This is a sum of following two image:
+ - https://github.com/wurstmeister/zookeeper-docker
+ - https://github.com/wurstmeister/kafka-docker
+ 
+So the credit goes mainly to their creators.
--- start-kafka.sh
+++ start-kafka.sh
@@ -32,6 +32,12 @@ term_handler() {
   exit
 }
 
+# Start ZK
+sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/log4j.properties
+sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
+
+/opt/zookeeper-${ZOOKEEPER_VERSION}/bin/zkServer.sh start
+
 
 # Capture kill requests to stop properly
 trap "term_handler" SIGHUP SIGINT SIGTERM
